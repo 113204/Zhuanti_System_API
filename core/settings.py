4d80bc7665a46400a518c878e08b87d6efb3d565
Epoch <10756022@ntub.edu.tw>
2024-10-09 14:35:01 +0800

    update: forget pass sent email account

--- core/settings.py
+++ core/settings.py
@@ -137,3 +137,10 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # Gmail的郵件服務地址
+EMAIL_PORT = 587  # 587是Gmail的TLS端口
+EMAIL_USE_TLS = True  # 啟用TLS
+EMAIL_HOST_USER = 'benchnoko@gmail.com'  # 您的Gmail地址
+EMAIL_HOST_PASSWORD = 'vyuc hukn nljx lgqs'  # 使用應用專用密碼
